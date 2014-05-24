75ac70343b018bef87666bee3f6d140bde541dcb
Paul Drummond <paul.drummond@iode.co.uk>
2014-05-24 11:38:05 +0100

    Initial commit

---
+++ src/bennumbergame/Main.java
@@ -0,0 +1,33 @@
+package bennumbergame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// STEP 1: Computer decides number between range 1-50.
+		// Example:
+		// - Computer chooses 35.
+		// STEP 2: Player chooses number between range 1-50
+		//Example:
+		// - Player types 20
+		// STEP 3: Computer tells player if answer is higher or lower than guess
+		//Example:
+		// - Computer displays: "INCORRECT! Try again"
+		// - Computer displays: "Tip: try a higher number"
+		// STEP 4: Play chooses number between range 1-50:
+		//Example:
+		// - Player chooses 35
+		// STEP 5: Computer tells player that it is correct!
+		//Example:
+		// - Computer displays: "CORRECT! Well done!"
+		// STEP 6: Computer let's player start again or exit
+		//Example:
+		// - Computer displays: "Play again (y/n):" 
+		
+		//New features:
+		
+		//PART X: Add levels with different ranges
+		
+	}
+
+}
